ee82a16b25265a2fc9a611b7af2a5bfc77b1f5bf
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-16 12:17:18 +0530

    Create  WithoutSemi.java

---
+++ WithoutSemi.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.lang.*;
+public class WithoutSemi
+   {
+    
+
+  public static void main(String[] args) 
+  
+  {
+    
+    int i = 0;
+    
+    if(System.out.printf("Sanfoundary!! ") == null) {}
+    
+    for(i = 1; i < 2; System.out.println("Hello World."))
+    
+    {
+      
+      i++;
+      
+    }
+    
+  }
+  
+}
